WrenSecurity/wrends

commit d750ad39895c2d72ab4a23524a46067edd108676

OPENDJ-1607 Merge HeartBeatConnectionFactory and AuthenticatedConnectionFactory into LDAPConnectionFactory
* reimplemented LDAPConnectionFactory so that it now includes all the
functionality previously provided by the HBCF and ACF factories

* initial heartbeat now uses SSL handshake, StartTLS, initial bind
request, or a heartbeat search request, depending on configuration

* renamed connection factory and listener options so that related
options group together.

opendj-ldap-toolkit/src/main/java/com/forgerock/opendj/ldap/tools/AuthRate.java
@@ -202,6 +202,7 @@ public final class AuthRate extends ConsoleApplication {
                     break;
                 }
 
+                final BindRequest bindRequest = getBindRequest();
                 if (bindRequest instanceof SimpleBindRequest) {
                     final SimpleBindRequest o = (SimpleBindRequest) bindRequest;
                     if (br == null) {
@@ -315,7 +316,6 @@ public final class AuthRate extends ConsoleApplication {
         private SearchScope scope;
         private DereferenceAliasesPolicy dereferencesAliasesPolicy;
         private String[] attributes;
-        private BindRequest bindRequest;
         private int invalidCredPercent;
 
         private BindPerformanceRunner(final PerformanceRunnerOptions options)
@@ -479,11 +479,11 @@ public final class AuthRate extends ConsoleApplication {
                 return 0;
             }
 
-            connectionFactory = connectionFactoryProvider.getConnectionFactory();
+            connectionFactory = connectionFactoryProvider.getUnauthenticatedConnectionFactory();
+            runner.setBindRequest(connectionFactoryProvider.getBindRequest());
             runner.validate();
 
-            runner.bindRequest = connectionFactoryProvider.getBindRequest();
-            if (runner.bindRequest == null) {
+            if (runner.getBindRequest() == null) {
                 throw new ArgumentException(LocalizableMessage
                         .raw("Authentication information must be provided to use this tool"));
             }

opendj-cli/src/main/java/com/forgerock/opendj/cli/AuthenticatedConnectionFactory.java
@@ -1,298 +1,0 @@
-/*
- * CDDL HEADER START
- *
- * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
- *
- * You can obtain a copy of the license at legal-notices/CDDLv1_0.txt
- * or http://forgerock.org/license/CDDLv1.0.html.
- * See the License for the specific language governing permissions
- * and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL HEADER in each
- * file and include the License file at legal-notices/CDDLv1_0.txt.
- * If applicable, add the following below this CDDL HEADER, with the
- * fields enclosed by brackets "[]" replaced with your own identifying
- * information:
- *      Portions Copyright [yyyy] [name of copyright owner]
- *
- * CDDL HEADER END
- *
- *
- *      Copyright 2009-2010 Sun Microsystems, Inc.
- *      Portions Copyright 2011-2015 ForgeRock AS.
- */
-package com.forgerock.opendj.cli;
-
-import static org.forgerock.util.Utils.*;
-
-import java.util.concurrent.atomic.AtomicReference;
-
-import org.forgerock.opendj.ldap.AbstractConnectionWrapper;
-import org.forgerock.opendj.ldap.Connection;
-import org.forgerock.opendj.ldap.ConnectionFactory;
-import org.forgerock.opendj.ldap.IntermediateResponseHandler;
-import org.forgerock.opendj.ldap.LdapException;
-import org.forgerock.opendj.ldap.LdapPromise;
-import org.forgerock.opendj.ldap.requests.BindRequest;
-import org.forgerock.opendj.ldap.responses.BindResult;
-import org.forgerock.util.AsyncFunction;
-import org.forgerock.util.Function;
-import org.forgerock.util.Reject;
-import org.forgerock.util.promise.ExceptionHandler;
-import org.forgerock.util.promise.Promise;
-import org.forgerock.util.promise.ResultHandler;
-/**
- * An authenticated connection factory can be used to create pre-authenticated
- * connections to a Directory Server.
- * <p>
- * The connections returned by an authenticated connection factory support all
- * operations with the exception of Bind requests. Attempts to perform a Bind
- * will result in an {@code UnsupportedOperationException}.
- * <p>
- * In addition, the returned connections support retrieval of the
- * {@code BindResult} returned from the initial Bind request, or last rebind.
- * <p>
- * Support for connection re-authentication is provided through the
- * {@link #setRebindAllowed} method which, if set to {@code true}, causes
- * subsequent connections created using the factory to support the
- * {@code rebind} method.
- * <p>
- * If the Bind request fails for some reason (e.g. invalid credentials), then
- * the connection attempt will fail and an {@link LdapException} will be thrown.
- */
-public final class AuthenticatedConnectionFactory implements ConnectionFactory {
-
-    /**
-     * An authenticated connection supports all operations except Bind
-     * operations.
-     */
-    public static final class AuthenticatedConnection extends AbstractConnectionWrapper<Connection> {
-
-        private final BindRequest request;
-        private volatile BindResult result;
-
-        private AuthenticatedConnection(final Connection connection, final BindRequest request,
-                final BindResult result) {
-            super(connection);
-            this.request = request;
-            this.result = result;
-        }
-
-        /*
-         * Bind operations are not supported by pre-authenticated connections.
-         * These methods will always throw {@code UnsupportedOperationException}.
-         */
-        /** {@inheritDoc} */
-        @Override
-        public BindResult bind(BindRequest request) throws LdapException {
-            throw new UnsupportedOperationException();
-        }
-
-        /** {@inheritDoc} */
-        @Override
-        public BindResult bind(String name, char[] password) throws LdapException {
-            throw new UnsupportedOperationException();
-        }
-
-        /** {@inheritDoc} */
-        @Override
-        public LdapPromise<BindResult> bindAsync(BindRequest request,
-            IntermediateResponseHandler intermediateResponseHandler) {
-            throw new UnsupportedOperationException();
-        }
-
-        /**
-         * Returns an unmodifiable view of the Bind result which was returned
-         * from the server after authentication.
-         *
-         * @return The Bind result which was returned from the server after
-         *         authentication.
-         */
-        public BindResult getAuthenticatedBindResult() {
-            return result;
-        }
-
-        /**
-         * Re-authenticates to the Directory Server using the bind request
-         * associated with this connection. If re-authentication fails for some
-         * reason then this connection will be automatically closed.
-         *
-         * @return A promise representing the result of the operation.
-         * @throws UnsupportedOperationException
-         *             If this connection does not support rebind operations.
-         * @throws IllegalStateException
-         *             If this connection has already been closed, i.e. if
-         *             {@code isClosed() == true}.
-         */
-        public LdapPromise<BindResult> rebindAsync() {
-            if (request == null) {
-                throw new UnsupportedOperationException();
-            }
-
-            return connection.bindAsync(request)
-                      .thenOnResult(new ResultHandler<BindResult>() {
-                          @Override
-                          public void handleResult(final BindResult result) {
-                              // Save the result.
-                              AuthenticatedConnection.this.result = result;
-                          }
-                      }).thenOnException(new ExceptionHandler<LdapException>() {
-                          @Override
-                          public void handleException(final LdapException exception) {
-                              /*
-                               * This connection is now unauthenticated so prevent further use.
-                               */
-                              connection.close();
-                          }
-                      });
-        }
-
-        /**
-         * Returns the string representation of this authenticated connection.
-         *
-         * @return The string representation of this authenticated connection factory.
-         */
-        @Override
-        public String toString() {
-            StringBuilder builder = new StringBuilder();
-            builder.append("AuthenticatedConnection(");
-            builder.append(connection);
-            builder.append(')');
-            return builder.toString();
-        }
-
-    }
-
-    private final BindRequest request;
-    private final ConnectionFactory parentFactory;
-    private boolean allowRebinds;
-
-    /**
-     * Creates a new authenticated connection factory which will obtain
-     * connections using the provided connection factory and immediately perform
-     * the provided Bind request.
-     *
-     * @param factory
-     *            The connection factory to use for connecting to the Directory
-     *            Server.
-     * @param request
-     *            The Bind request to use for authentication.
-     * @throws NullPointerException
-     *             If {@code factory} or {@code request} was {@code null}.
-     */
-    public AuthenticatedConnectionFactory(final ConnectionFactory factory, final BindRequest request) {
-        Reject.ifNull(factory, request);
-        this.parentFactory = factory;
-
-        // FIXME: should do a defensive copy.
-        this.request = request;
-    }
-
-    @Override
-    public void close() {
-        parentFactory.close();
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public Connection getConnection() throws LdapException {
-        final Connection connection = parentFactory.getConnection();
-        BindResult bindResult = null;
-        try {
-            bindResult = connection.bind(request);
-        } finally {
-            if (bindResult == null) {
-                connection.close();
-            }
-        }
-
-        /*
-         * If the bind didn't succeed then an exception will have been thrown
-         * and this line will not be reached.
-         */
-        return new AuthenticatedConnection(connection, request, bindResult);
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public Promise<Connection, LdapException> getConnectionAsync() {
-        final AtomicReference<Connection> connectionHolder = new AtomicReference<>();
-        return parentFactory.getConnectionAsync()
-            .thenAsync(
-                    new AsyncFunction<Connection, BindResult, LdapException>() {
-                        @Override
-                        public Promise<BindResult, LdapException> apply(final Connection connection)
-                                throws LdapException {
-                            connectionHolder.set(connection);
-                            return connection.bindAsync(request);
-                        }
-                    }
-            ).then(
-                    new Function<BindResult, Connection, LdapException>() {
-                        @Override
-                        public Connection apply(BindResult result) throws LdapException {
-                            // FIXME: should make the result unmodifiable.
-                            return new AuthenticatedConnection(connectionHolder.get(), request, result);
-                        }
-                    },
-                    new Function<LdapException, Connection, LdapException>() {
-                        @Override
-                        public Connection apply(LdapException errorResult) throws LdapException {
-                            closeSilently(connectionHolder.get());
-                            throw errorResult;
-                        }
-                    }
-            );
-    }
-
-    /**
-     * Indicates whether or not rebind requests are to be supported by
-     * connections created by this authenticated connection factory.
-     * <p>
-     * Rebind requests are invoked using the connection's {@code rebind} method
-     * which will throw an {@code UnsupportedOperationException} if rebinds are
-     * not supported (the default).
-     *
-     * @return allowRebinds {@code true} if the {@code rebind} operation is to
-     *         be supported, otherwise {@code false}.
-     */
-    boolean isRebindAllowed() {
-        return allowRebinds;
-    }
-
-    /**
-     * Specifies whether or not rebind requests are to be supported by
-     * connections created by this authenticated connection factory.
-     * <p>
-     * Rebind requests are invoked using the connection's {@code rebind} method
-     * which will throw an {@code UnsupportedOperationException} if rebinds are
-     * not supported (the default).
-     *
-     * @param allowRebinds
-     *            {@code true} if the {@code rebind} operation is to be
-     *            supported, otherwise {@code false}.
-     * @return A reference to this connection factory.
-     */
-    AuthenticatedConnectionFactory setRebindAllowed(final boolean allowRebinds) {
-        this.allowRebinds = allowRebinds;
-        return this;
-    }
-
-    /**
-     * Returns the string representation of this authenticated connection factory.
-     *
-     * @return The string representation of this authenticated connection factory.
-     */
-    @Override
-    public String toString() {
-        final StringBuilder builder = new StringBuilder();
-        builder.append("AuthenticatedConnectionFactory(");
-        builder.append(parentFactory);
-        builder.append(')');
-        return builder.toString();
-    }
-
-}

opendj-cli/src/main/java/com/forgerock/opendj/cli/ConnectionFactoryProvider.java
@@ -27,10 +27,13 @@
 package com.forgerock.opendj.cli;
 
 import static com.forgerock.opendj.cli.ArgumentConstants.*;
-import static com.forgerock.opendj.cli.CliMessages.*;
 import static com.forgerock.opendj.cli.CliConstants.DEFAULT_LDAP_PORT;
+import static com.forgerock.opendj.cli.CliMessages.*;
 import static com.forgerock.opendj.cli.Utils.getHostNameForLdapUrl;
-import static org.forgerock.opendj.ldap.LDAPConnectionFactory.*;
+import static org.forgerock.opendj.ldap.LDAPConnectionFactory.AUTHN_BIND_REQUEST;
+import static org.forgerock.opendj.ldap.LDAPConnectionFactory.CONNECT_TIMEOUT;
+import static org.forgerock.opendj.ldap.LDAPConnectionFactory.SSL_CONTEXT;
+import static org.forgerock.opendj.ldap.LDAPConnectionFactory.SSL_USE_STARTTLS;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -65,6 +68,7 @@ import org.forgerock.opendj.ldap.requests.GSSAPISASLBindRequest;
 import org.forgerock.opendj.ldap.requests.PlainSASLBindRequest;
 import org.forgerock.opendj.ldap.requests.Requests;
 import org.forgerock.util.Options;
+import org.forgerock.util.time.Duration;
 
 /**
  * A connection factory designed for use with command line tools.
@@ -134,8 +138,6 @@ public final class ConnectionFactoryProvider {
 
     /**  The basic connection factory. */
     private ConnectionFactory connFactory;
-    /** The authenticated connection factory. */
-    protected ConnectionFactory authenticatedConnFactory;
 
     /** The bind request to connect with. */
     private BindRequest bindRequest;
@@ -345,15 +347,34 @@ public final class ConnectionFactoryProvider {
     }
 
     /**
-     * Checks if any conflicting arguments are present, build the connection with selected arguments and returns the
-     * connection factory. If the application is interactive, it will prompt the user for missing parameters.
+     * Constructs a connection factory for pre-authenticated connections. Checks if any conflicting arguments are
+     * present, build the connection with selected arguments and returns the connection factory. If the application is
+     * interactive, it will prompt the user for missing parameters.
      *
      * @return The connection factory.
      * @throws ArgumentException
-     *             If an error occurs during the parsing of the arguments.
-     *             (conflicting arguments or if an error occurs during building SSL context).
+     *         If an error occurs during the parsing of the arguments (conflicting arguments or if an error occurs
+     *         during building SSL context).
      */
-    public ConnectionFactory getConnectionFactory() throws ArgumentException {
+    public ConnectionFactory getAuthenticatedConnectionFactory() throws ArgumentException {
+        return getConnectionFactory(true);
+    }
+
+    /**
+     * Constructs a connection factory for unauthenticated connections. Checks if any conflicting arguments are present,
+     * build the connection with selected arguments and returns the connection factory. If the application is
+     * interactive, it will prompt the user for missing parameters.
+     *
+     * @return The connection factory.
+     * @throws ArgumentException
+     *         If an error occurs during the parsing of the arguments (conflicting arguments or if an error occurs
+     *         during building SSL context).
+     */
+    public ConnectionFactory getUnauthenticatedConnectionFactory() throws ArgumentException {
+        return getConnectionFactory(false);
+    }
+
+    private ConnectionFactory getConnectionFactory(boolean usePreAuthentication) throws ArgumentException {
         if (connFactory == null) {
             port = portArg.isPresent() ? portArg.getIntValue() : 0;
 
@@ -415,13 +436,14 @@ public final class ConnectionFactoryProvider {
             }
 
             Options options = Options.defaultOptions();
-
             if (sslContext != null) {
                 options.set(SSL_CONTEXT, sslContext)
-                    .set(USE_STARTTLS, useStartTLSArg.isPresent());
+                    .set(SSL_USE_STARTTLS, useStartTLSArg.isPresent());
             }
-            options.set(CONNECT_TIMEOUT_IN_MILLISECONDS,
-                TimeUnit.MILLISECONDS.toMillis(getConnectTimeout()));
+            options.set(CONNECT_TIMEOUT, new Duration((long) getConnectTimeout(), TimeUnit.MILLISECONDS));
+            if (usePreAuthentication) {
+                options.set(AUTHN_BIND_REQUEST, getBindRequest());
+            }
             connFactory = new LDAPConnectionFactory(hostNameArg.getValue(), port, options);
         }
         return connFactory;
@@ -500,24 +522,6 @@ public final class ConnectionFactoryProvider {
                             .getLongIdentifier());
             throw new ArgumentException(message);
         }
-    }
-
-    /**
-     * Returns the authenticated connection factory.
-     *
-     * @return The authenticated connection factory.
-     * @throws ArgumentException
-     *             If an error occurs during parsing the arguments.
-     */
-    public ConnectionFactory getAuthenticatedConnectionFactory() throws ArgumentException {
-        if (authenticatedConnFactory == null) {
-            authenticatedConnFactory = getConnectionFactory();
-            final BindRequest bindRequest = getBindRequest();
-            if (bindRequest != null) {
-                authenticatedConnFactory = new AuthenticatedConnectionFactory(authenticatedConnFactory, bindRequest);
-            }
-        }
-        return authenticatedConnFactory;
     }
 
     /**

opendj-core/src/main/java/org/forgerock/opendj/ldap/AuthenticatedConnectionFactory.java
@@ -1,181 +1,0 @@
-/*
- * CDDL HEADER START
- *
- * The contents of this file are subject to the terms of the
- * Common Development and Distribution License, Version 1.0 only
- * (the "License").  You may not use this file except in compliance
- * with the License.
- *
- * You can obtain a copy of the license at legal-notices/CDDLv1_0.txt
- * or http://forgerock.org/license/CDDLv1.0.html.
- * See the License for the specific language governing permissions
- * and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL HEADER in each
- * file and include the License file at legal-notices/CDDLv1_0.txt.
- * If applicable, add the following below this CDDL HEADER, with the
- * fields enclosed by brackets "[]" replaced with your own identifying
- * information:
- *      Portions Copyright [yyyy] [name of copyright owner]
- *
- * CDDL HEADER END
- *
- *
- *      Copyright 2009-2010 Sun Microsystems, Inc.
- *      Portions Copyright 2011-2015 ForgeRock AS.
- */
-
-package org.forgerock.opendj.ldap;
-
-import java.util.concurrent.atomic.AtomicReference;
-
-import org.forgerock.opendj.ldap.requests.BindRequest;
-import org.forgerock.opendj.ldap.responses.BindResult;
-import org.forgerock.util.AsyncFunction;
-import org.forgerock.util.Function;
-import org.forgerock.util.promise.Promise;
-
-import static org.forgerock.util.Utils.*;
-
-/**
- * An authenticated connection factory can be used to create pre-authenticated
- * connections to a Directory Server.
- * <p>
- * The connections returned by an authenticated connection factory support all
- * operations with the exception of Bind requests. Attempts to perform a Bind
- * will result in an {@code UnsupportedOperationException}.
- * <p>
- * If the Bind request fails for some reason (e.g. invalid credentials), then
- * the connection attempt will fail and an {@link LdapException} will be thrown.
- */
-final class AuthenticatedConnectionFactory implements ConnectionFactory {
-
-    /**
-     * An authenticated connection supports all operations except Bind
-     * operations.
-     */
-    public static final class AuthenticatedConnection extends AbstractConnectionWrapper<Connection> {
-
-        private AuthenticatedConnection(final Connection connection) {
-            super(connection);
-        }
-
-        /**
-         * Bind operations are not supported by pre-authenticated connections.
-         * These methods will always throw {@code UnsupportedOperationException}.
-         */
-        public LdapPromise<BindResult> bindAsync(final BindRequest request,
-                final IntermediateResponseHandler intermediateResponseHandler,
-                final LdapResultHandler<? super BindResult> resultHandler) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public BindResult bind(BindRequest request) throws LdapException {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public BindResult bind(String name, char[] password) throws LdapException {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder builder = new StringBuilder();
-            builder.append("AuthenticatedConnection(");
-            builder.append(connection);
-            builder.append(')');
-            return builder.toString();
-        }
-
-    }
-
-    private final BindRequest request;
-    private final ConnectionFactory parentFactory;
-
-    /**
-     * Creates a new authenticated connection factory which will obtain
-     * connections using the provided connection factory and immediately perform
-     * the provided Bind request.
-     *
-     * @param factory
-     *            The connection factory to use for connecting to the Directory
-     *            Server.
-     * @param request
-     *            The Bind request to use for authentication.
-     */
-    AuthenticatedConnectionFactory(final ConnectionFactory factory, final BindRequest request) {
-        this.parentFactory = factory;
-
-        // FIXME: should do a defensive copy.
-        this.request = request;
-    }
-
-    @Override
-    public void close() {
-        // Delegate.
-        parentFactory.close();
-    }
-
-    @Override
-    public Connection getConnection() throws LdapException {
-        final Connection connection = parentFactory.getConnection();
-        boolean bindSucceeded = false;
-        try {
-            connection.bind(request);
-            bindSucceeded = true;
-        } finally {
-            if (!bindSucceeded) {
-                connection.close();
-            }
-        }
-
-        /*
-         * If the bind didn't succeed then an exception will have been thrown
-         * and this line will not be reached.
-         */
-        return new AuthenticatedConnection(connection);
-    }
-
-    @Override
-    public Promise<Connection, LdapException> getConnectionAsync() {
-        final AtomicReference<Connection> connectionHolder = new AtomicReference<>();
-        return parentFactory.getConnectionAsync()
-            .thenAsync(
-                    new AsyncFunction<Connection, BindResult, LdapException>() {
-                        @Override
-                        public Promise<BindResult, LdapException> apply(final Connection connection)
-                                throws LdapException {
-                            connectionHolder.set(connection);
-                            return connection.bindAsync(request);
-                        }
-                    }
-            ).then(
-                    new Function<BindResult, Connection, LdapException>() {
-                        @Override
-                        public Connection apply(BindResult result) throws LdapException {
-                            return new AuthenticatedConnection(connectionHolder.get());
-                        }
-                    },
-                    new Function<LdapException, Connection, LdapException>() {
-                        @Override
-                        public Connection apply(LdapException error) throws LdapException {
-                            closeSilently(connectionHolder.get());
-                            throw error;
-                        }
-                    }
-            );
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder builder = new StringBuilder();
-        builder.append("AuthenticatedConnectionFactory(");
-        builder.append(parentFactory);
-        builder.append(", ");
-        builder.append(request);
-        builder.append(')');
-        return builder.toString();
-    }
-}

commit e794dd049a0cb9e153941db23fd3b7692e9eb6a2

OPENDJ-2468 Fix authrate tool
The tool was broken since d750ad39895.
Problem was that we tried to rebind with the bind request template in
the PerformanceRunner (see changes done in PerformanceRunner in
d750ad39895).
We do not want to rebind in authrate tool.
To fix that, a field bindRequestTemplate has been added to the
AuthRate.BindPerformanceRunner.
This prevent us to use the bindRequest needed by the other tool to
perform rebinds.

opendj-ldap-toolkit/src/main/java/com/forgerock/opendj/ldap/tools/AuthRate.java
@@ -202,7 +202,7 @@ public final class AuthRate extends ConsoleApplication {
                     break;
                 }
 
-                final BindRequest bindRequest = getBindRequest();
+                final BindRequest bindRequest = bindRequestTemplate;
                 if (bindRequest instanceof SimpleBindRequest) {
                     final SimpleBindRequest o = (SimpleBindRequest) bindRequest;
                     if (br == null) {
@@ -317,10 +317,16 @@ public final class AuthRate extends ConsoleApplication {
         private DereferenceAliasesPolicy dereferencesAliasesPolicy;
         private String[] attributes;
         private int invalidCredPercent;
+        /** Template of the bind requests which will be send to the remote server. */
+        private BindRequest bindRequestTemplate;
 
         private BindPerformanceRunner(final PerformanceRunnerOptions options)
                 throws ArgumentException {
             super(options);
+        }
+
+        private void setBindRequestTemplate(final BindRequest bindRequestTemplate) {
+            this.bindRequestTemplate = bindRequestTemplate;
         }
 
         @Override
@@ -480,13 +486,12 @@ public final class AuthRate extends ConsoleApplication {
             }
 
             connectionFactory = connectionFactoryProvider.getUnauthenticatedConnectionFactory();
-            runner.setBindRequest(connectionFactoryProvider.getBindRequest());
-            runner.validate();
-
-            if (runner.getBindRequest() == null) {
-                throw new ArgumentException(LocalizableMessage
-                        .raw("Authentication information must be provided to use this tool"));
+            final BindRequest bindRequestTemplate = connectionFactoryProvider.getBindRequest();
+            if (bindRequestTemplate == null) {
+                throw new ArgumentException(ERR_AUTHRATE_NO_BIND_DN_PROVIDED.get());
             }
+            runner.setBindRequestTemplate(bindRequestTemplate);
+            runner.validate();
         } catch (final ArgumentException ae) {
             argParser.displayMessageAndUsageReference(getErrStream(), ERR_ERROR_PARSING_ARGS.get(ae.getMessage()));
             return ResultCode.CLIENT_SIDE_PARAM_ERROR.intValue();

opendj-ldap-toolkit/src/main/resources/com/forgerock/opendj/ldap/tools/tools.properties
@@ -505,6 +505,8 @@ ERR_TOOL_NOT_ENOUGH_ITERATIONS=%s argument must be greater than or equal to %s \
 ERR_TOOL_ARG_NEEDED_WHEN_USING_ARG=%s must be used when using %s
 ERR_TOOL_ARG_MUST_BE_USED_WHEN_ARG_CONDITION=%s must be used if %s is %s
 INFO_TOOL_WARMING_UP=Warming up for %d seconds...
+ERR_AUTHRATE_NO_BIND_DN_PROVIDED=Authentication information must be provided \
+ to use this tool
  #
  # MakeLDIF tool
  #